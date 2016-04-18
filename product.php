f8524b82b122d9ab90402394bb0cc7efb38ea122
Clown13 <jeff_sanha@hotmail.com>
2016-04-18 16:41:58 -0600

    Update and rename cars.php to product.php

---
+++ product.php
@@ -0,0 +1,46 @@
+<?php include("Header.php"); ?>
+
+        
+<div id="main" align="center">            
+
+    <h1>Select your Product</h1>
+    <h2>Select options from the drop down menu</h2>
+
+    <form name="formSubmit" action="product_searcher.php" method="post">
+           
+            <select name="category" class="gap">
+                <?php 
+                $sql = mysqli_query($con, "SELECT DISTINCT CATEGORY FROM product");
+                echo '<option value="">--Type--</option>';
+                while ($row = mysqli_fetch_array($sql)){
+                echo '<option>'.$row['CATEGORY'].'</option>';
+                }
+                ?>
+            </select>  
+
+            <select name="price" class="gap">
+                <?php 
+                $sql = mysqli_query($con, "SELECT DISTINCT PRICE FROM product");
+                echo '<option value="">--Seats--</option>';
+                while ($row = mysqli_fetch_array($sql)){
+                echo '<option>'.$row['PRICE'].'</option>';
+                }
+                ?>
+            </select>  
+
+            <select name="stock" class="gap">
+                <?php 
+                $sql = mysqli_query($con, "SELECT *  FROM product WHERE stock >0");
+                echo '<option value="">--Stock--</option>';
+                while ($row = mysqli_fetch_array($sql)){
+                echo '<option>'.$row['STOCK'].'</option>';
+                }
+                ?>
+            </select>  
+
+
+        <p><input class="submitButton" type="submit" value="Find car">
+    </form>
+          
+</div>
+<?php include("Footer.php"); ?>
