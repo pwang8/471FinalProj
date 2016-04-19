1b301822348aa9f0aca9d0ed863c8990e149b96d
pwang8 <p.wang8@outlook.com>
2016-04-18 19:31:11 -0600

    Update product_add_function.php
    added 'addProduct' capabilities

--- product_add_function.php
+++ product_add_function.php
@@ -1,18 +1,17 @@
 <?php
-
 include("Header.php");
-
-$availability = $_POST["availability"];
 $category = $_POST["category"];
 $price = $_POST["price"];
 $stock = $_POST["stock"];
-
-$sql= "INSERT INTO product(VIN,AVAILABILITY,CATEGORY,PRICE,STOCK) VALUES ('','$availability','$category','$price','$stock')";
+$name = $_POST["name"];
+$description = $_POST["desc"];
+$image = $_POST["imageUrl"];
+$sql= "INSERT INTO product(product_id, product_image, product_name, stock, price, description)
+        VALUES ('NULL', '".$image."', '".$name."', '".$stock."', '".$price."', '".$description."')";
 if (!mysqli_query($con,$sql))
 {
 die('Error: ' . mysqli_error($con));
 }
 else
 echo "1 record added";
-
 include("Footer.php");
